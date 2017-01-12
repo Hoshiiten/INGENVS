a25fe0b03427778ebfda760a3e3cd2c98706644c
Xhya <alexia.souvane@gmail.com>
2017-01-12 17:45:20 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [ -z "$1" ] || [ -z "$2" ]
+then
+        echo "Must be: bash run.sh [ idneo4j ] [ passwordNeo4j ]"
+	exit 1
+fi
+
 python prototype/neo4j_scripts/diseaseNodes.py $1 $2
 python prototype/neo4j_scripts/diseaseEdges.py $1 $2
 python prototype/neo4j_scripts/geneNodes.py $1 $2
