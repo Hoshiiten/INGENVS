0c9878e9ed8f1a1136eb00e32819871ca6d0b699
Xhya <alexia.souvane@gmail.com>
2017-01-13 15:50:03 +0100

    Update geneNodes.py

--- prototype/neo4j_scripts/geneNodes.py
+++ prototype/neo4j_scripts/geneNodes.py
@@ -9,10 +9,10 @@ import py2neo as gp
 from py2neo.ogm import *
 import sys
 
-id = sys.argv[1]
+ID = sys.argv[1]
 password = sys.argv[2]
 
-gp.authenticate("localhost:7474","neo4j","felix")
+gp.authenticate("localhost:7474",ID,password)
 graph = gp.Graph()
 
 class Gene(GraphObject):
